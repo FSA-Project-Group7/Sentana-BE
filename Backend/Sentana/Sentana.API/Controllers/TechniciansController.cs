269ce12b73994c96ceb1b5695563175ff2d6fe1a
devnam1401 <namphhe180085@fpt.edu.vn>
2026-03-04 14:34:33 +0700

    Implement view technician

---
+++ Backend/Sentana/Sentana.API/Controllers/TechniciansController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sentana.API.DTOs.Technician;
+using Sentana.API.Helpers;
+using Sentana.API.Services;
+
+namespace Sentana.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Manager")]
+    public class TechniciansController : ControllerBase
+    {
+        private readonly ITechnicianService _technicianService;
+
+        public TechniciansController(ITechnicianService technicianService)
+        {
+            _technicianService = technicianService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllTechnician()
+        {
+            try
+            {
+                var technicians = await _technicianService.GetAllTechnician();
+                var response = ApiResponse<IEnumerable<TechnicianResponseDto>>.Success(technicians);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                var errorResponse = ApiResponse<IEnumerable<TechnicianResponseDto>>.Fail(500, $"Đã xảy ra lỗi khi lấy danh sách kĩ thuật viên: ex.Message");
+                return StatusCode(500, errorResponse);
+            }
+        }
+    }
+}
---
+++ Backend/Sentana/Sentana.API/DTOs/Technician/TechnicianResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using Sentana.API.Enums;
+
+namespace Sentana.API.DTOs.Technician
+{
+    public class TechnicianResponseDto
+    {
+        public int AccountId { get; set; }
+        public string? UserName { get; set; }
+        public string? FullName { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public GeneralStatus? Status { get; set; }
+        public byte? TechAvailability { get; set; }
+    }
+}
