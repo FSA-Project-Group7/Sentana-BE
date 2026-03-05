7ace04c542be18156df176e88208628b4df4e8c4
HieuTQT <hieudeptrai2910@gmail.com>
2026-03-05 20:42:43 +0700

    Fix Validate Birthdate

--- Backend/Sentana/Sentana.API/DTOs/Auth/UpdateProfileRequestDto.cs
+++ Backend/Sentana/Sentana.API/DTOs/Auth/UpdateProfileRequestDto.cs
@@ -23,8 +23,8 @@ namespace Sentana.API.DTOs.Auth
         [Required(ErrorMessage = "Địa chỉ không được để trống.", AllowEmptyStrings = false)]
         public string Address { get; set; } = string.Empty;
 
-
         [Required(ErrorMessage = "Ngày sinh không được để trống.")]
+        [PastDate(ErrorMessage = "Bạn chưa thể sinh ra ở tương lai được! Vui lòng chọn ngày hợp lệ.")]
         public DateTime BirthDay { get; set; }
     }
 }
