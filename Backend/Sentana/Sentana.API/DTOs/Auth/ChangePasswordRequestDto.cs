2ee375434f036a6d93b86af897a8aedea689147e
HieuTQT <hieudeptrai2910@gmail.com>
2026-03-06 15:05:34 +0700

    fix validate change / reset password

--- Backend/Sentana/Sentana.API/DTOs/Auth/ChangePasswordRequestDto.cs
+++ Backend/Sentana/Sentana.API/DTOs/Auth/ChangePasswordRequestDto.cs
@@ -5,17 +5,17 @@ namespace Sentana.API.DTOs.Auth
 {
     public class ChangePasswordRequestDto
     {
-        [Required(ErrorMessage = "Vui lòng nhập mã OTP từ Email.")]
+        [Required(ErrorMessage = "Vui lòng nhập mã OTP từ Email.", AllowEmptyStrings = false)]
         public string OtpCode { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Vui lòng nhập mật khẩu cũ.")]
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu cũ.", AllowEmptyStrings = false)]
         public string OldPassword { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Vui lòng nhập mật khẩu mới.")]
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu mới.", AllowEmptyStrings = false)]
         [RegularExpression(ValidationHelper.PasswordRegex, ErrorMessage = "Mật khẩu phải từ 8 ký tự, gồm ít nhất 1 chữ, 1 số và 1 ký tự đặc biệt.")]
         public string NewPassword { get; set; } = string.Empty;
 
-        [Required(ErrorMessage = "Vui lòng xác nhận lại mật khẩu mới.")]
+        [Required(ErrorMessage = "Vui lòng xác nhận lại mật khẩu mới.", AllowEmptyStrings = false)]
         [Compare("NewPassword", ErrorMessage = "Mật khẩu xác nhận không khớp.")]
         public string ConfirmNewPassword { get; set; } = string.Empty;
     }
