be38d723d9d1dc8af08cc3b321e14b4a5237e850
HieuTQT <hieudeptrai2910@gmail.com>
2026-03-06 14:57:31 +0700

    fix reset password

--- Backend/Sentana/Sentana.API/DTOs/Auth/ResetPasswordRequestDto.cs
+++ Backend/Sentana/Sentana.API/DTOs/Auth/ResetPasswordRequestDto.cs
@@ -8,6 +8,7 @@ namespace Sentana.API.DTOs.Auth
         public string Email { get; set; } = string.Empty;
         public string OtpCode { get; set; } = string.Empty;
 
+        [Required(ErrorMessage =" Mật khẩu không được để trống", AllowEmptyStrings = false)]
         [RegularExpression(ValidationHelper.PasswordRegex, ErrorMessage = "Mật khẩu phải từ 8 ký tự, gồm ít nhất 1 chữ, 1 số và 1 ký tự đặc biệt.")]
         public string NewPassword { get; set; } = string.Empty;
     }
