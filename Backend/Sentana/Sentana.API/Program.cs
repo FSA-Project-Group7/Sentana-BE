365a4abc12ddbf4181c0a8411f082d3370e70190
devnam1401 <namphhe180085@fpt.edu.vn>
2026-04-08 09:55:05 +0700

    config minio

--- Backend/Sentana/Sentana.API/Program.cs
+++ Backend/Sentana/Sentana.API/Program.cs
@@ -76,9 +76,13 @@ namespace Sentana.API
 				});
             builder.Services.AddSingleton<IMinioClient>(sp =>
             {
+                var endpoint = Environment.GetEnvironmentVariable("MINIO_ENDPOINT") ?? "localhost:9000";
+                var accessKey = Environment.GetEnvironmentVariable("MINIO_ACCESS_KEY") ?? "minioadmin";
+                var secretKey = Environment.GetEnvironmentVariable("MINIO_SECRET_KEY") ?? "minioadmin";
+
                 return new MinioClient()
-                    .WithEndpoint("localhost:9000")
-                    .WithCredentials("minioadmin", "minioadmin")
+                    .WithEndpoint(endpoint)
+                    .WithCredentials(accessKey, secretKey)
                     .Build();
             });
 
